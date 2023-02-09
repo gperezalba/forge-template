dca41e7f6941056afcc22b9dacc6a5f3c0c8bcd7
gperezalba <gperezalba94@gmail.com>
2023-02-09 18:33:13 +0100

    feat(all) deploy using config.json

--- test/Fixture.sol
+++ test/Fixture.sol
@@ -7,24 +7,14 @@ import {Utils} from "../script/Utils.s.sol";
 import {DeployScript} from "../script/Deploy.s.sol";
 import {Deployer} from "../src/Deployer.sol";
 
-string constant ROOT_PATH = "./addresses/";
-
 contract Fixture is Test, Utils {
     DeployScript public deployScript;
 
     Deployer public deployer;
-    address public ms = readAddressFromFile(ROOT_PATH, _getChainID(), "MS.txt");
+    address public ms = getAddressFromConfigJson(".MS");
 
     constructor() {
         deployScript = new DeployScript();
         deployer = deployScript.deployDeployer();
-    }
-
-    function _getChainID() internal view returns (uint256) {
-        uint256 id;
-        assembly {
-            id := chainid()
-        }
-        return id;
     }
 }
