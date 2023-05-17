b194fc9c80462b61b81a9c5a1a02a5fd2a484b0c
gperezalba <gperezalba94@gmail.com>
2023-05-17 18:18:33 +0200

    feat(all) update to solc 0.8.19

--- test/Fixture.sol
+++ test/Fixture.sol
@@ -1,8 +1,8 @@
 // SPDX-License-Identifier: UNLICENSED
-pragma solidity ^0.8.13;
+pragma solidity 0.8.19;
 
-import "forge-std/Script.sol";
-import "forge-std/Test.sol";
+import {Script} from "forge-std/Script.sol";
+import {Test} from "forge-std/Test.sol";
 import {Utils} from "../script/Utils.s.sol";
 import {DeployScript} from "../script/Deploy.s.sol";
 import {Deployer} from "../src/Deployer.sol";
